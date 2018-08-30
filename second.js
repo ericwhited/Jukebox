ed6613074c25e615f09e1f0268aaa2ff540c8dc4
Eric Whited <whited.j.eric@gmail.com>
2018-08-30 10:32:05 -0400

    added more

---
+++ second.js
@@ -0,0 +1,51 @@
+// var Jukebox = {
+// 	song: document.querySelector('#myAudio'),
+// 	playButton: document.querySelector('#play'),
+// 	play: function() {
+// 		Jukebokox.song.play();
+// 	}
+
+
+// playButton.addEventListener('click', function() {myAudio.play(); })
+
+	// pause: function() {
+
+	// } 
+	
+	// skip: function() {
+
+	// }
+
+	// stop: function() {
+
+	// }
+// }
+
+
+
+
+function Jukebox(play, pause, next, previous) {
+	this.play = function() {alert('hi');};
+	this.song = song;
+
+
+//  var play = document.querySelector('play');
+// }
+}
+
+var newJuk = new Jukebox('in2deep');
+
+newJuk.play
+
+
+
+
+
+
+
+
+
+// function Songs(songTitle, songSrc) {
+// 	this.songTitle = {};
+// 	this.songSrc = {};
+// }
